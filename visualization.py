1a2688fdb24eb024cd6cd7c3793cb16aa1ae37ad
pelinbalkaya <pelinbalkaya01@gmail.com>
2024-02-12 13:16:27 +0300

    modify visualization.py

--- visualization.py
+++ visualization.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
-
+import numpy as np
 from sklearn.metrics import confusion_matrix, classification_report
 
 import nltk
@@ -9,7 +9,15 @@ nltk.download('punkt')
 nltk.download('wordnet')
 
 
-def get_sentence_word_count(text_list):
+def get_sentence_word_count(text_list:list) -> tuple[int, int]:
+    """generates sentences and word counts for a given list
+
+    Args:
+        text_list (list): given list of texts
+
+    Returns:
+        (int, int): sentence and unique word count
+    """
     sent_count = 0
     word_count = 0
     vocab = {}
@@ -29,19 +37,37 @@ def get_sentence_word_count(text_list):
     return sent_count, word_count
 
 
-def show_categories(data_categories, title):
+def show_categories(data_categories:list, title:str)->None:
+    """prints category names and the number of inscriptions for it
+
+    Args:
+        data_categories (list): list of categories
+        title (str): name of each category 
+    """
     print(f"{'='*10}{title}{'='*10}")
     for i, (cat_name, data_category) in enumerate(data_categories):
         print(f"Cat:{i + 1} {cat_name} : {len(data_category)}")
 
 
-def count_plot(data_categories):
+def count_plot(data_categories:list)->None:
+    """shows plot of each categories frequency
+
+    Args:
+        data_categories (list): list of categories
+    """
     plt.figure(figsize=(10,10))
     sns.countplot(y='medical_specialty', data=data_categories)
     plt.show()
 
 
-def classification_results(labels, y_true, y_pred):
+def classification_results(labels:list, y_true:np.ndarray, y_pred:np.ndarray)->None:
+    """shows confusion matrix of the predictions and prints classification report 
+
+    Args:
+        labels (list): class label names
+        y_true (np.ndarray): true names of categories
+        y_pred (np.ndarray): category predictions
+    """
     cm = confusion_matrix(y_true, y_pred)
 
     fig = plt.figure(figsize=(20,20))
